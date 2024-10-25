8363848c964fb73d312b4d28c54c14100bc4fa73
Mark McGuire <mcguirenumber1@gmail.com>
2024-10-25 00:03:06 -0700

    added routing to individual html pages

--- src/blog.js
+++ src/blog.js
@@ -5,34 +5,43 @@ var blogs = [
         title: "Title 1",
         date: "10/24/24",
         description: "First blog post",
-        image: "./images/image1.png",
+        image: "src/images/image1.jpeg",
         imageAlt: "first image",
-        slug: "post1",
+        slug: "post-one",
     },
     {
         title: "Title 2",
         date: "10/24/24",
         description: "Second blog post",
-        image: "./images/image2.png",
+        image: "src/images/image2.jpeg",
         imageAlt: "second image",
-        slug: "post2",
+        slug: "post-two",
     },
 ];
 // Iterate through posts and create a div with associated html elements to create previews
 function blogPosts() {
     var blogContainer = document.getElementById("blog-container");
     blogs.forEach(function (post) {
+        // Create elements
         var postContainer = document.createElement("div");
         var postTitle = document.createElement("h2");
         var description = document.createElement("p");
         var image = document.createElement("img");
+        var route = document.createElement("a");
+        // Set element contents
         postTitle.textContent = post.title;
         description.textContent = post.description;
         image.src = post.image;
+        image.width = 225;
+        image.height = 225;
         image.alt = post.imageAlt;
+        route.href = "blog/" + post.slug + ".html";
+        route.innerHTML = "<br /> Read more...";
+        // Append these elements to the div and then to the element in the dom
         postContainer.appendChild(postTitle);
         postContainer.appendChild(description);
         postContainer.appendChild(image);
+        postContainer.appendChild(route);
         blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.append(postContainer);
     });
 }
--- src/blog.ts
+++ src/blog.ts
@@ -16,7 +16,7 @@ const blogs: Blog[] = [
     title: "Title 1",
     date: "10/24/24",
     description: "First blog post",
-    image: "./images/image1.png",
+    image: "src/images/image1.jpeg",
     imageAlt: "first image",
     slug: "post-one",
   },
@@ -24,7 +24,7 @@ const blogs: Blog[] = [
     title: "Title 2",
     date: "10/24/24",
     description: "Second blog post",
-    image: "./images/image2.png",
+    image: "src/images/image2.jpeg",
     imageAlt: "second image",
     slug: "post-two",
   },
@@ -36,21 +36,28 @@ function blogPosts() {
   const blogContainer = document.getElementById("blog-container");
 
   blogs.forEach((post) => {
+    // Create elements
     const postContainer = document.createElement("div");
     const postTitle = document.createElement("h2");
     const description = document.createElement("p");
     const image = document.createElement("img");
     const route = document.createElement("a");
 
+    // Set element contents
     postTitle.textContent = post.title;
     description.textContent = post.description;
     image.src = post.image;
+    image.width = 225;
+    image.height = 225;
     image.alt = post.imageAlt;
-    route.href = "blog/" + post.slug + ".html"
+    route.href = "blog/" + post.slug + ".html";
+    route.innerHTML = "<br /> Read more...";
 
+    // Append these elements to the div and then to the element in the dom
     postContainer.appendChild(postTitle);
     postContainer.appendChild(description);
     postContainer.appendChild(image);
+    postContainer.appendChild(route);
 
     blogContainer?.append(postContainer);
   });
