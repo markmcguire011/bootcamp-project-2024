66a7dfdb2d205fbd12f0391a8e493d15d478315f
Mark McGuire <mcguirenumber1@gmail.com>
2024-11-07 22:40:16 -0800

    feat: created blogPreview component

---
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function BlogPreview({
+  name,
+  description,
+  image,
+  posted,
+  slug,
+}: {
+  name: string;
+  description: string;
+  image: string;
+  posted: string;
+  slug: string;
+}) {
+  return (
+    <div className="my-5 w-max items-center text-center border-2 border-black">
+      <div className="pt-2">
+        <Link className="text-xl font-semibold" href={"blogs/" + slug}>
+          {name}
+        </Link>
+      </div>
+      <div className="py-2">
+        <Image src={image} width={300} height={300} alt="Main Picture" />
+        <div className="pt-2">
+          <p className="font-semibold">{description}</p>
+          <p className="font-semibold">{posted}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
