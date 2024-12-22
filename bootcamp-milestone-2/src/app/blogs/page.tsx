79b9e83df5e383725ea5f1cb8562454c1e6172a8
Mark McGuire <mcguirenumber1@gmail.com>
2024-12-22 13:48:17 -0800

    feat(blogs): integrated getBlogs() for mongoDB into blogs page

--- bootcamp-milestone-2/src/app/blogs/page.tsx
+++ bootcamp-milestone-2/src/app/blogs/page.tsx
@@ -1,19 +1,37 @@
 import BlogPreview from "@/components/blogPreview";
-import blogs from "@/app/blogData";
+import Blog from "@/database/blogSchema";
+import connectDB from "@/database/db";
 
-export default function Blogs() {
+async function getBlogs() {
+  await connectDB(); // function from db.ts before
+
+  try {
+    // query for all blogs and sort by date
+    const blogs = await Blog.find().sort({ date: -1 }).orFail();
+    // send a response as the blogs as the message
+    return blogs;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function Blogs() {
+  const blogs = await getBlogs();
+
   return (
     <div className="px-40 py-20 min-h-screen">
-      {blogs.map((blog) => (
-        <BlogPreview
-          name={blog.title}
-          description={blog.description}
-          image={blog.image}
-          posted={blog.date}
-          slug={blog.slug}
-          key={blog.title}
-        />
-      ))}
+      {blogs
+        ? blogs.map((blog) => (
+            <BlogPreview
+              name={blog.title}
+              description={blog.description}
+              image={blog.image}
+              posted={blog.date}
+              slug={blog.slug}
+              key={blog.title}
+            />
+          ))
+        : null}
     </div>
   );
 }
