62adc3a6f17abb5767b9d8d25bac158c9187208a
Mark McGuire <mcguirenumber1@gmail.com>
2024-12-24 15:49:22 +0100

    feat(blog views): changed styling & layout of blogPreview and blog page

--- bootcamp-milestone-2/src/components/blog.tsx
+++ bootcamp-milestone-2/src/components/blog.tsx
@@ -21,10 +21,10 @@ export default function BlogPage({ blog }: { blog: Blog }) {
     <div className="my-5 w-max flex flex-col gap-2">
       <h1 className="text-4xl font-bold">{blog.title}</h1>
       <p className="font-semibold">{parseCommentTime(blog.date)}</p>
-      <Image src={blog.image} width={300} height={300} alt={blog.imageAlt ? blog.imageAlt : "alt"} />
+      <Image src={blog.image} width={300} height={300} alt={blog.imageAlt} />
       <div className="py-2">
         <div className="pt-2">
-          <p>{blog.text}</p>
+          <p>{blog.content}</p>
         </div>
       </div>
     </div>
--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Blog } from "@/database/blogSchema";
+import { parseCommentTime } from "./blog";
 
 export default function BlogPreview({ blog }: { blog: Blog }) {
   // const options = { year: "numeric", month: "long", day: "numeric" };
@@ -9,22 +10,21 @@ export default function BlogPreview({ blog }: { blog: Blog }) {
 
   return (
     <Link href={"blogs/" + blog.slug}>
-      <div className="my-5 w-max items-center text-center border-2 border-black rounded">
-        <div className="pt-2">
-          <h1 className="text-xl font-semibold">{blog.title}</h1>
-        </div>
-        <div className="py-2">
+      <div className="my-5 w-max h-min items-center text-center border-2 border-black rounded flex gap-4">
+        <div className="">
           <Image
             src={blog.image}
-            width={300}
-            height={300}
-            alt={blog.imageAlt ? blog.imageAlt : "alt"}
+            width={100}
+            height={100}
+            alt={blog.imageAlt}
           />
-          <div className="pt-2">
-            <p className="font-semibold">{blog.description}</p>
-            <p className="font-semibold">{blog.date.toDateString()}</p>
-          </div>
         </div>
+        <div className="">
+          <h1 className="text-xl font-semibold">{blog.title}</h1>
+          <p className="font-semibold">{blog.description}</p>
+        </div>
+        <div className="h-8 w-[2px] bg-black rounded-md"></div>
+        <p className="font-semibold pr-2">{parseCommentTime(blog.date)}</p>
       </div>
     </Link>
   );
